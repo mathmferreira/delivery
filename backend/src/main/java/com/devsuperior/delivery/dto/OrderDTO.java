ef1b583ae6f1e9c1badd6769339352124dd38d95
Matheus Maia <mathmferreira@hotmail.com>
2021-01-06 15:59:32 -0300

    GET /orders

---
+++ backend/src/main/java/com/devsuperior/delivery/dto/OrderDTO.java
@@ -0,0 +1,45 @@
+package com.devsuperior.delivery.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.devsuperior.delivery.entities.Order;
+import com.devsuperior.delivery.entities.enums.OrderStatus;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+@SuppressWarnings("serial")
+public class OrderDTO implements Serializable {
+
+	private Long id;
+
+	private String address;
+
+	private Double latitude;
+
+	private Double longitude;
+
+	private Instant moment;
+
+	private OrderStatus status;
+	
+	private List<ProductDTO> products = new ArrayList<>();
+
+	public OrderDTO(Order entity) {
+		id = entity.getId();
+		address = entity.getAddress();
+		latitude = entity.getLatitude();
+		longitude = entity.getLongitude();
+		moment = entity.getMoment();
+		status = entity.getStatus();
+		products = entity.getProducts().stream().map(ProductDTO::new).collect(Collectors.toList());
+	}
+	
+
+}
--- backend/src/main/java/com/devsuperior/delivery/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/delivery/repositories/OrderRepository.java
@@ -1,11 +1,19 @@
 package com.devsuperior.delivery.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.delivery.entities.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+	@Query("Select Distinct obj From Order obj "
+			+ "Join Fetch obj.products "
+			+ "Where obj.status = 0 Order By obj.moment ASC")
+	public List<Order> findOrdersWithProducts();
 
 }
