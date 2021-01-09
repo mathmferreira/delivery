e2011d44a5e6f29dac8b9518d5e49b2bdfdd1ea9
Matheus Maia <mathmferreira@hotmail.com>
2021-01-09 19:42:47 -0300

    Add Routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      <Routes />
     </>
   );
 }
